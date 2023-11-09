101983510150bc66dd4f0fc51e0d0ab18deb90a5
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-11-09 15:33:21 +0600

    z index problem slove

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Partner from "@/components/partner/Partner";
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto">
-      <section className="bg-[#0f1115] sticky top-0">
+      <section className="bg-[#0f1115] sticky top-0 z-50">
       <Navbar/>
       </section>
       <section className="sticky top-0 -z-40 w-full ">
